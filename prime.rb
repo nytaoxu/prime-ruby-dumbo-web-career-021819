149e81704cf64c2f27617cd90756f86720df2f5e
William Xu <nytaoxu@gmail.com>
2019-02-09 21:18:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number <= 0 || number == 1
+    return false
+  end
+  result = true
+  index = 2
+  while index < number && index * index <= number
+    if number % index == 0
+      result = false
+      break
+    end
+    index += 1
+  end
+  result
+end
